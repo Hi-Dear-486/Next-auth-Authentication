a77ffc9172685e6e7646aff9ffe76be71cc1d048
Muhammad zeeshan <fullstackdeveloper486@gmail.com>
2024-10-26 04:43:21 +0500

    update title or desc

--- app/page.js
+++ app/page.js
@@ -7,16 +7,14 @@ const Home = () => {
       <div class="container px-5 py-32 mx-auto">
         <div class="text-center ">
           <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">
-            "Streamlining User Authentication in Next.js with NextAuth.js"
+            "Streamlining User Authentication in Next.js with NextAuth.js or Google Cloud"
           </h1>
           <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">
-            NextAuth.js is a flexible authentication library for Next.js
-            applications. It simplifies user authentication by supporting
-            various providers (like Google, GitHub, and email) and offers
-            built-in session management. With minimal configuration, developers
-            can secure routes and handle user sessions seamlessly. Its
-            adaptability makes it suitable for both small projects and
-            large-scale applications.
+            Implementing authentication in a Next.js application can be
+            effectively achieved using NextAuth.js, which provides a simple way
+            to manage user authentication. You can also integrate Google Cloud
+            services for managing your application, but here, I’ll focus on
+            setting up authentication using NextAuth.js with a Google provider.
           </p>
           <div class="flex mt-6 justify-center">
             <div class="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
